7bf1eaa35d343cebd9566566f8b3f78c43563d39
SiddhantJha317 <siddhantjha365@gmail.com>
2022-08-23 15:14:21 +0530

    markdown for projects

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -4,4 +4,6 @@ st.set_page_config(page_title="Siddhant_Jha",page_icon='👩‍💻')
 
 st.title('Hi i am Siddhant')
 
-st.sidebar.success(' ')
+st.subheader(" Hi welcome, are you new to this place my name is Siddhant i am an aspiring data scientist working on various projects")
+st.subheader(" across the fields of Machine Learning and also Deep learning , i also like to participate amon various ML competetions ,both online and offline in the recent past.") 
+st.subheader(" Other than that this website of mine conatins all of my work which you can check on the Projects page and also feel free to Contact me personally or on public platfroms from the Contact page")
---
+++ pages/3_📧_Contact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Contact Me')
+
+st.subheader('')
